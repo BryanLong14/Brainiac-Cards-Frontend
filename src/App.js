bbc5ed9c74a578c6d76470921633527d91fa34a5
Bryan Long <BryanLong1@gmail.com>
2018-02-01 15:13:16 -0700

    Flashcard Additions to User cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // Import required React Bootstrap components within src/App.js file or your  component files:
 // import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import HighSchoolFlashcards from "./components/HighSchoolFlashcards";
 import UserFlashCards from "./components/UserFlashCards";
@@ -10,58 +10,46 @@ var databaseURL = "https://sleepy-sea-27116.herokuapp.com";
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      HighSchoolFlashcards: [],
-      UserFlashCards: []
-    };
-    this.HighSchoolFlashcardCreateSubmitHandler = this.HighSchoolFlashcardCreateSubmitHandler.bind(this);
-    this.HighSchoolFlashcardUpdateSubmitHandler = this.HighSchoolFlashcardUpdateSubmitHandler.bind(this);
-    this.HighSchoolFlashcardListSubmitHandler = this.HighSchoolFlashcardListSubmitHandler.bind(this);
-    this.HighSchoolFlashcardDeleteSubmitHandler = this.HighSchoolFlashcardDeleteSubmitHandler.bind(this);
-    this.listAllHighSchoolFlashcardsSubmitHandler = this.listAllHighSchoolFlashcardsSubmitHandler.bind(this);
-    this.UserFlashCardCreateSubmitHandler = this.UserFlashCardCreateSubmitHandler.bind(this);
-    this.UserFlashCardUpdateSubmitHandler = this.UserFlashCardUpdateSubmitHandler.bind(this);
-    this.UserFlashCardListSubmitHandler = this.UserFlashCardListSubmitHandler.bind(this);
-    this.UserFlashCardDeleteSubmitHandler = this.UserFlashCardDeleteSubmitHandler.bind(this);
-    this.listAllUserFlashCardsSubmitHandler = this.listAllUserFlashCardsSubmitHandler.bind(this);
-    // which of the above methods do we need to bind to this? Research...
+      HighSchoolFlashcards: {},
+      UserFlashCards: {}
+      }
+      this.getFlashcardData=this.getFlashcardData.bind(this)
   }
 
   getFlashcardData() {
     fetch(databaseURL)
       .then(response => response.json())
       .then(response => {
-        this.setState({
-          HighSchoolFlashcards: response.HighSchoolFlashcards,
-          UserFlashCards: response.UserFlashCards
-        });
-        console.log(response.highschool_flashcards[8])
+        console.log(response)
         console.log(response.highschool_flashcards)
+        console.log(response.highschool_flashcards[8])
+        console.log(response.highschool_flashcards[8].word)
+        this.setState({
+          HighSchoolFlashcards: response.highschool_flashcards,
+          UserFlashCards: response.teachers_flashcards
+        });
       })
       .catch(err => console.error(err));
   }
 
   componentDidMount() {
-    console.log(this.state)
-    // The above line logs empty arrays, so the response is not being passed correctly
     this.getFlashcardData();
   }
 
 
 
-
-
-
-
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Brainiac Cards</h1>
-        </header>
-        <HighSchoolFlashcards />
-        <UserFlashCards />
+    return ( <
+      div className = "App" >
+      <header className = "App-header" >
+      <h1 className = "App-title" > Brainiac Cards < /h1> < /
+      header >
+      <HighSchoolFlashcards
+      HighSchoolFlashcards={this.state.HighSchoolFlashcards}
+      / >
+      <UserFlashCards / >
       </div>
     );
   }
--- src/components/HighSchoolFlashcards.js
+++ src/components/HighSchoolFlashcards.js
@@ -3,9 +3,49 @@ import React from "react";
 function HighSchoolFlashcards(props) {
   console.log(props.data);
   return(
-    <h2>100 Words Every High School Graduate Should Know</h2>
+    <div className="singleFlashcard">
+      <h2>100 Words Every High School Graduate Should Know</h2>
+    </div>
   )
 }
 
 
 export default HighSchoolFlashcards;
+
+
+// Shuffler Function Below
+// import React from 'react';
+// import { Link } from 'react-router';
+//
+// function shuffleArray(array) {
+//   let i = array.length - 1;
+//   for (; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     const temp = array[i];
+//     array[i] = array[j];
+//     array[j] = temp;
+//   }
+//   return array;
+// }
+//
+// function RecommendedPosts({ posts }) {
+//   const shuffledPosts = shuffleArray(posts);
+//   return (
+//     <ul>
+//       {shuffledPosts.map((post, idx) => {
+//         return (
+//           <li key={idx}>
+//             <p>{post.title}</p>
+//             <p>{post.text}</p>
+//             <p>{post.category}</p>
+//             <Link to={`/blog/post-1/:${post.id}`}>Weiter lesen</Link>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// }
+// RecommendedPosts.propTypes = {
+//   posts: React.PropTypes.array,
+// };
+// export default RecommendedPosts;
