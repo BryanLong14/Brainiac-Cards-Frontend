48faaee86e3badd818747d2b8b214cc653ca06dc
Bryan Long <BryanLong1@gmail.com>
2018-04-04 13:52:01 -0600

    FIxed Cypress Tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,24 +1,24 @@
 describe("Braniac Cards", function() {
   it("should work", function() {
     cy.visit("http://localhost:3000/");
+    cy.get("Button"),
+      { multiple: true }
+        .click()
+        .eq(0)
+        .should("contain", "100 Words Every High School Graduate Should Know");
     cy
-      .get("Button"), {multiple:true}
-      .click()
+      .get("btn-lg")
       .eq(0)
-      .should("contain", "100 Words Every High School Graduate Should Know");
-    // cy
-    //   .get("btn-lg")
-    //   .eq(0)
-    //   .click();
-    // cy
-    //   .get("input")
-    //   .eq(0)
-    //   .type("Test");
-    // cy.get("#add-button").click();
-    // cy.wait(500);
-    // cy
-    //   .get(".modal-open")
-    //   .eq(2)
-    //   .click()
+      .click();
+    cy
+      .get("input")
+      .eq(0)
+      .type("Test");
+    cy.get("#add-button").click();
+    cy.wait(500);
+    cy
+      .get(".modal-open")
+      .eq(2)
+      .click();
   });
 });
